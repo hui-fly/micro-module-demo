796cf2cd0b316eac798e5a97f3969910839e3a54
zhoudonghui <zhoudonghui@qutoutiao.net>
2020-04-01 15:49:19 +0800

    feat: 修复

--- config/apiConfig.js
+++ config/apiConfig.js
@@ -1,42 +1,22 @@
 module.exports = {
     project: '119', // 项目鉴权project, 测试用 md-admin
     title: '统一运营平台XXX模块', // 项目名称
-    production: { // 生产环境
-        qukan: { // 接口1project
-            'default': '//api.1sapp.com/' // 默认分支
-        },
-        perm: { // 接口2project
-            'default': '//api.perm.1sapp.com/'
-        },
+    production: { 
         task: {
-            'default': "http://admin-coin-task-server.1sapp.com/"
+            'default': "xxx.com/"
         }
     },
     preview: { // 预发布环境
-        qukan: {
-            'default': '//pre.api.1sapp.com/'
-        },
-        perm: {
-            'default': '//api.perm.1sapp.com/'
-        },
         task: {
-            'default': "http://admin-coin-task-server.1sapp.com/"
+            'default': "xxx.com/"
         }
     },
     development: { // 测试环境
-        qukan: {
-            'default': '//test-api.qukan.qttcs3.cn/',
-            '1': '//test1-api.qukan.qttcs3.cn/' // 测试分支1
-        },
-        perm: {
-            'default': '//test-api.qukan.perm.qttcs3.cn/',
-            // 'default': '//api.perm.1sapp.com/'
-        },
         YApi: {
             'default': '//'
         },
         task: {
-            'default': "http://qa-coin-services-task.qttcs3.cn/"
+            'default': "xxx.com/"
         }
     }
 }
